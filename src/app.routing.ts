c165168c1e4843347ea9abe4d1efd04de0004533
Slawomir Grzebyk <grzebyk.slawomir@gmail.com>
2016-12-30 14:46:25 +0100

    First Commit with Router

---
+++ src/app.routing.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by oem on 2016-12-30.
+ */
+
+import {Router, RouterModule} from '@angular/router';
+
+import {AboutComponent} from './app/about/about.component';
+import {SearchComponent} from './app/search/search.component';
+import {ContactComponent} from './app/contact/contact.component';
+
+
+export const routing = RouterModule.forRoot([
+  {path:'', component: SearchComponent},
+  {path:'about', component: AboutComponent},
+  {path:'home', component: SearchComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'**', component: AboutComponent},
+]);
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import  {routing} from 'app.routing';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
